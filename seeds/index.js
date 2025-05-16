51bebda01af92ff3895cc62312e812cf5aa0b52e
daredevil-10 <vipuly41@gmail.com>
2025-05-16 15:15:44 +0530

    Completed the basic CRUD and basic styling

--- seeds/index.js
+++ seeds/index.js
@@ -18,13 +18,20 @@ const seedDB = async ()=>{
     for(let i = 0 ; i<50; i++)
     {
         const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground({
             location:`${cities[random1000].city},${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam blanditiis at ipsa corporis eaque eos eligendi consectetur excepturi libero earum illo accusamus fugit, asperiores nam exercitationem magni dolor amet cum!',
+            price 
 
         });
         await camp.save();
     }
 }
 
-seedDB();
+seedDB().then(()=>{
+    mongoose.connection.close();
+    console.log("connection closed");
+});
